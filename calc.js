6943adf2cbac89ab1bec6bc7ead481294dd15202
Eduardo <eduardo.carneiro2907@gmail.com>
2022-04-11 13:00:47 -0300

    Display clicked numbers on screen
    The number buttons now dispares a event when clicked, that calls the
    'storeNumber()', which stores the number in the 'operation' variable and
    display it on calculator screen, with 'updateScreen()' function.
    
    Also switched functions positions for better readability.

--- calc.js
+++ calc.js
@@ -1,12 +1,42 @@
+const number_buttons = [
+    document.getElementById('zero'),
+    document.getElementById('one'),
+    document.getElementById('two'),
+    document.getElementById('three'),
+    document.getElementById('four'),
+    document.getElementById('five'),
+    document.getElementById('six'),
+    document.getElementById('seven'),
+    document.getElementById('eight'),
+    document.getElementById('nine'),
+]
+number_buttons.forEach(button => {
+    button.addEventListener('click', function (event) {
+        storeNumber(number_buttons.indexOf(button))
+    })
+})
+let operation = ''
+storeNumber = (algarism) => {
+    operation += String(algarism)
+    updateScreen(operation)
+}
+const screen = document.querySelector("#screen > span") 
+updateScreen =  operation => {
+    screen.innerText = operation
+}
+
 function add(num1, num2) {
-    return num1+num2
+    return num1 + num2
 }
 function subtract(num1, num2) {
-    return num1-num2
+    return num1 - num2
 }
 function multiply(num1, num2) {
-    return num1*num2
+    return num1 * num2
 }
 function divide(num1, num2) {
-    return num1/num2
+    return num1 / num2
+}
+function operate(operator, num1, num2,) {
+    return operator(num1, num2)
 }
